7b1ed9595cc9830e54867955d616a88ec837db42
Ivan Silvestre <isilvestreneto@gmail.com>
2022-06-21 16:41:17 -0300

    Initial commit

---
+++ crud-spring/src/main/java/com/ivan/crudspring/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.ivan.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ivan.crudspring.model.Course;
+import com.ivan.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CourseController {
+
+    private final CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+}
